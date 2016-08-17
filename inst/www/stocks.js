a5a72ea56c1cc8c225da374ebb53a51577ca7d71
fcluzeau <francois.cluzeau@gmail.com>
2016-08-17 23:10:14 +0200

    Update stocks.js

--- inst/www/stocks.js
+++ inst/www/stocks.js
@@ -224,7 +224,7 @@ Ext.onReady(function() {
   function updatemenu(){
     var data = Ext.getCmp('workspace-panel').getActiveTab().data;
     if(data){
-      Ext.getCmp("action").picker.setValue(data.portefe);
+      Ext.getCmp("action").setValue(data.portefe);
       Ext.getCmp("startdate").picker.setValue(data.start);
       Ext.getCmp("enddate").picker.setValue(data.end);
       Ext.getCmp("graphtype").setValue(data.type);
@@ -241,7 +241,6 @@ Ext.onReady(function() {
     var from = Ext.getCmp("startdate").picker.getValue();
     var to = Ext.getCmp("enddate").picker.getValue();
     var type = Ext.getCmp("graphtype").getValue();
-    var portefe = Ext.getCmp("action").getValue();
     var current = Ext.getCmp("currentBtn").pressed;
     var gain = Ext.getCmp("currentBtn").pressed;
     var moyenne = Ext.getCmp("moyenneBtn").pressed;
