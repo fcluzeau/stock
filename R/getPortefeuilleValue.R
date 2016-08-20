5bf0e1a05019193878c0ab21beb275b0d5fc5994
fcluzeau <francois.cluzeau@gmail.com>
2016-08-20 15:39:59 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -15,7 +15,7 @@ myporte[j,2]<-as.numeric(myporte[j,2])+as.numeric(mydata$Close[j]);
 
 myporte[,1]<-mydata$Date;
 colnames(myporte)<-c("Date","Close");
-myporte<-as.data.frame(myporte);
+myporte<-data.frame(myporte);
 myporte[is.na(myporte)] <- 0;
   ase1 <- myporte[1,2];
   ase2<- myporte[m,2];
