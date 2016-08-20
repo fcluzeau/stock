890dfecbaf1af3507e9ce229cf742b7086991b22
fcluzeau <francois.cluzeau@gmail.com>
2016-08-20 15:13:55 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -30,6 +30,7 @@ gainf[(i/20)]<-gaini[i];
 moyenneredm<-round(mean(100*gainf),5);
 moyennegeoredm<-100*getMoyenneGeometrique(gainf);
 moyennegeoredm<-round(moyennegeoredm,5);
+mydata$Close<-myporte[,2];
 
-qplot(Date, Close, data = myporte, geom = c("line", "smooth"), xlab=paste("Gain du Capital:",gain,"%","; moyenne arithmétique mensuelle du rendement:", moyenneredm,"%","; moyenne géométrique mensuelle du rendement:", moyennegeoredm,"%"))
+qplot(Date, Close, data = mydata, geom = c("line", "smooth"), xlab=paste("Gain du Capital:",gain,"%","; moyenne arithmétique mensuelle du rendement:", moyenneredm,"%","; moyenne géométrique mensuelle du rendement:", moyennegeoredm,"%"))
 }
