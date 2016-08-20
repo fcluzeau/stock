0dd4c734d74fb1e490f98d59811706ac4087849d
fcluzeau <francois.cluzeau@gmail.com>
2016-08-20 15:57:12 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -25,7 +25,7 @@ mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
   
   gainf<-numeric(floor((m-1)/20));
 for(i in 1:(m-1)){
-gaini[i]<- 100*((myporte[i,2]-myporte[i+1,2])/myporte[i,2]);
+gaini[i]<- 100*((mydata[i,2]-mydata[i+1,2])/mydata[i,2]);
 if((i/20)==floor(i/20)){
 gainf[(i/20)]<-gaini[i];
 }}
