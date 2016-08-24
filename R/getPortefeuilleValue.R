774b451319d84da9dc25cae0c3e32c46ebbce16d
fcluzeau <francois.cluzeau@gmail.com>
2016-08-24 08:49:51 +0200

    Update getPortefeuilleValue.R

--- R/getPortefeuilleValue.R
+++ R/getPortefeuilleValue.R
@@ -12,7 +12,6 @@ if(length(nomb)==0){
 for(i in 1:n){
 mydat <- yahoodata(portefeu[i], from, to);
 ai<-floor(li/mydat$Close[m]);
-mydat<-c(mydat$Date,mydat$Close);
 for(j in 1:m){
 mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
 
@@ -23,7 +22,6 @@ mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
 else{
 for(i in 1:n){
 mydat <- yahoodata(portefeu[i], from, to);
-mydat<-c(mydat$Date, mydat$Close);
 ai<-floor(10000*as.numeric(nomb[i])/mydat[m,2]);
 for(j in 1:m){
 mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
