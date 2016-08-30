2cbeea0960cb815f538b4e0b211e27996123941c
fcluzeau <francois.cluzeau@gmail.com>
2016-08-30 07:02:52 +0000

    Create getPorte.R

---
+++ R/getPorte.R
@@ -0,0 +1,30 @@
+getPorte<-function(portefe="AC.PA ACA.PA",nomb="75 25", from = "2013-01-01", to=Sys.time()){
+portefeu<-unlist(strsplit(portefe, " "));
+nomb<-unlist(strsplit(nomb, " "));
+m<-dim(yahoodata(portefeu[1], from, to))[1];
+mydata<-yahoodata(portefeu[1], from, to);
+mydata$Colse<-0;
+n<-length(portefeu);
+li<-1000000/n;
+
+
+if(length(nomb)==0){
+for(i in 1:n){
+mydat <- yahoodata(portefeu[i], from, to);
+ai<-floor(li/mydat$Close[m]);
+for(j in 1:m){
+mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
+
+}
+}
+}
+
+else{
+for(i in 1:n){
+mydat <- yahoodata(portefeu[i], from, to);
+ai<-floor(10000*as.numeric(nomb[i])/mydat[m,2]);
+for(j in 1:m){
+mydata$Close[j]<-as.numeric(mydata$Close[j])+ai*as.numeric(mydat$Close[j]);
+}}}
+
+return(mydata)}
