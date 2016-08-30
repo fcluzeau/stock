7823b5a2fe8e63fec3383efc9997826f6d17193d
fcluzeau <francois.cluzeau@gmail.com>
2016-08-30 07:05:32 +0000

    Update getMoyenne.R

--- R/getMoyenne.R
+++ R/getMoyenne.R
@@ -1,7 +1,12 @@
-getMoyenne <- function(ticker="GOOG",from = "2013-01-01", to=Sys.time()){
+getMoyenne <- function(ticker="GOOG",portefe="ACA.PA AC.PA", nomb="75 25", from = "2013-01-01", to=Sys.time()){
+     if(tickeer!="portefeuille"){
      mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     moyenne<-mean(mydata$Value);
     moyenne<-round(moyenne,5);
     return(moyenne);
 }
+
+else{
+porte<-getPorte(portefe, nomb, from, to)
+return(round(mean(porte$Close),5));}
