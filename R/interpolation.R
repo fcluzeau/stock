62446d57866e0bab54710762e8f1d829e7f41393
fcluzeau <francois.cluzeau@gmail.com>
2016-08-31 21:17:08 +0000

    Update interpolation.R

--- R/interpolation.R
+++ R/interpolation.R
@@ -1,6 +1,6 @@
 interpolation<-function(ticker = "GOOG", portefe="ACA.PA AC.PA",nomb="75 25", from = "2013-01-01", to=Sys.time(), n="3"){
 if(length(n)==0){
-n<-"1";}
+n<-1}
 n<-as.numeric(n)
 
 if(ticker!="portefeuille"){
