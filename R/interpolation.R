9a2506b24e8b9be6fa96cc89e419d3e2e0461368
fcluzeau <francois.cluzeau@gmail.com>
2016-08-31 19:25:27 +0000

    Update interpolation.R

--- R/interpolation.R
+++ R/interpolation.R
@@ -2,11 +2,11 @@ interpolation<-function(ticker = "GOOG", portefe="ACA.PA AC.PA",nomb="75 25", fr
 if(length(n)==0){
 n<-"1";}
 n<-as.numeric(n);
+mydat<-numeric(dim(mydata)[1]);
 if(ticker!="portefeuille"){
 mydata <- yahoodata(ticker, from, to);
 for(i in 1:dim(mydata)[1]){
-mydata$Date<-dateToChart(mydata$Date);
-mydata$Date[i]<-(i-1);}
-fit <- lm(mydata$Value ~ poly(mydata$Date, n, raw=TRUE));
+mydat[i]<-(i-1);}
+fit <- lm(mydata$Value ~ poly(mydat, n, raw=TRUE));
 }
-plot(mydata$Date, mydata$Value, type="l", lwd=floor(n/10))+points(mydata$Date, predict(fit), type="l", col="blue", lwd=floor(n/10))}
+plot(mydat, mydata$Value, type="l", lwd=floor(n/10))+points(mydata$Date, predict(fit), type="l", col="blue", lwd=floor(n/10))}
