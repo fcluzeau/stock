fb190e13c246569d8c68cb7978d5b587c382bfd3
fcluzeau <francois.cluzeau@gmail.com>
2016-08-31 10:53:50 +0000

    Update getVariance.R

--- R/getVariance.R
+++ R/getVariance.R
@@ -1,10 +1,7 @@
 getVariance <- function(ticker="GOOG", portefe="ACA.PA AC.PA", nomb="75 25", from = "2013-01-01", to=Sys.time()){
-    if(ticker!="portefeuille"){ mydata <- yahoodata(ticker, from, to);
+    mydata <- yahoodata(ticker, from, to);
     names(mydata) <- c("Symbol","Value","Date","Time","Name");
     var<-var(mydata$Value);
     var<-round(var,5);
     return(var);
 }
-else{
-porte<-getPorte(portefe, nomb, from, to)
-return(round(var(porte$Close),5));}
