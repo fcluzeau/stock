0c9731957073d033f29aeee8669930964102365d
fcluzeau <francois.cluzeau@gmail.com>
2016-08-31 22:34:39 +0000

    Update smoothplot.R

--- R/smoothplot.R
+++ R/smoothplot.R
@@ -10,8 +10,8 @@
 #' @export
 smoothplot <- function(portefe="AC.PA ACA.PA", nomb="75 25",deg="3", ticker = "GOOG", from = "2013-01-01", to=Sys.time()){
 if( ticker!= "portefeuille"){
+if(length(deg)==0){deg<-30}
 deg<-as.numeric(deg);
-if(deg==0){deg==30}
 mydata <- yahoodata(ticker, from, to);
   vol<-volatilite(ticker, from, to);
   qplot(Date, Close, data = mydata, xlab=paste("Votilité de l'action ",vol),ylab= ticker)+geom_smooth(span = deg/100);  
