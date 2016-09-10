1906ff9fd5dc006e52f33202f10064e3f8486705
fcluzeau <francois.cluzeau@gmail.com>
2016-09-10 12:11:36 +0200

    Update getCapitalGainMonth.R

--- R/getCapitalGainMonth.R
+++ R/getCapitalGainMonth.R
@@ -6,10 +6,10 @@ num<-dim(mydata)[1];
 gain<-matrix(ncol=2,nrow=num-1);
 gainf<-matrix(ncol=2,nrow=floor((num-1)/20));
 for(i in 1:num-1){
-gain[i,1]<- 100*((mydata[i,2]-mydata[i+1,2])/mydata[i,2]);
-gain[i,2]<-mydata[i+1,3];
 if((i/20)==floor(i/20)){
-gainf[(i/20),]<-gain[i,];
+gaini[i/20,1]<- 100*((mydata[i+1,2]-mydata[i,2])/mydata[i+1,2]);
+gaini[i/20,2]<-mydata[i+1,3];
+
 }}
 gainf<-as.data.frame(gainf);
 colnames(gainf)<-c("Value","Date");
